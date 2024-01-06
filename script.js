0dfeb8e2883bb47125090c19cdfced491c03a824
amritkundan <66768804+amritkundan@users.noreply.github.com>
2024-01-06 16:17:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var imageIndex = 0;
-    var images = ["firstImage.jpg", "secondImage.jpg", "thirdImage.jpg"]; // Add more images
+    var images = ["firstImage.png", "secondImage.png", "thirdImage.png"]; // Add more images
 
     function changeImage() {
         imageIndex = (imageIndex + 1) % images.length;
